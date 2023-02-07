7a156c556ffa770c73f3283747008fde16f8b7cf
Tanner <troxs690@gmail.com>
2023-02-07 13:31:17 -0600

    d

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -22,10 +22,29 @@ class BirdsController < ApplicationController
     end
   end
 
+  def update
+ bird = Bird.find(params[:id])
+ if bird
+  bird.update(bird_params)
+  render json: bird
+ else
+  render json: {error: "ERROR"}
+  end
+end
+
+
+def increment_likes
+bird = bird.find(params[:id])
+if bird
+ bird.update(likes: bird.likes +1)
+ render json: bird
+else
+  render json: {error: "ERROR"}
+end
   private
 
   def bird_params
-    params.permit(:name, :species)
+    params.permit(:name, :species, :likes)
   end
 
 end
